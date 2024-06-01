f0e74eda3a211b3ec147c18a0900e5a4186b7225
Jhonier Santana <jhonier_2504@hotmail.com>
2024-05-31 19:50:56 -0500

    Ejercicios del punto 2 hechos

---
+++ Exercise-2/Exercises.js
@@ -0,0 +1,24 @@
+const a = 4;
+const b = 7;
+const c = 2 * b;
+const x = a;
+const y = 3 * x;
+const u = 6;
+const w = 10;
+const z = 2 * w;
+
+// A.
+resultado = a / (b * c);
+console.log("A.", resultado);
+
+// B.
+resultado2 = Math.pow(a, 2) + Math.pow(b, 92); // a ** 2 + b ** 92
+console.log("B.", resultado2);
+
+// C.
+resultado3 = x + y / (u + w / a);
+console.log("C.", resultado3);
+
+// D.
+resultado4 = (x / y) * (z + w);
+console.log("D.", resultado4);
